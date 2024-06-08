8df5b60c9db6d1b368a4768e5e62dfd1973d259e
skygreenxd <dubov_2005@mail.ru>
2024-06-09 00:31:36 +0300

    2321

--- Руководство/TIERLIST.cs
+++ Руководство/TIERLIST.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Data;
-using System.Data.SqlClient;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.Windows.Forms;
@@ -9,35 +8,14 @@ namespace Руководство
 {
     public partial class TIERLIST : Form
     {
-        private CharacterRepository characterRepository;
-        private imageMove imagemove;
-        Database database = new Database();
+        private TierlistManager tierlistManager;
+
         public TIERLIST()
         {
             InitializeComponent();
-            characterRepository = new CharacterRepository(database);
-            imagemove = new imageMove(tierListTable, image_container);
-            tierListTable.Focus();
-            typeof(TableLayoutPanel).GetProperty("DoubleBuffered", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance).SetValue(tierListTable, true, null);
-            limitation();
-            selectChar();
-        }
-        protected void selectChar()
-        {
-            DataTable charImage = characterRepository.Getpic();
-
-            foreach (DataRow row in charImage.Rows)
-            {
-                string imagePath = row["image"].ToString(); // Получает путь к изображению из базы данных
-                PictureBox pictureBox = new PictureBox();
-                pictureBox.Image = Image.FromFile(imagePath); // Устанавливает изображение в PictureBox
-                pictureBox.SizeMode = PictureBoxSizeMode.Zoom; // Устанавливает режим отображения изображения
-                pictureBox.Width = 118; // Устанавливает ширину PictureBox
-                pictureBox.Height = 118; // Устанавливает высоту PictureBox
-                pictureBox.BorderStyle = BorderStyle.FixedSingle;
-                image_container.Controls.Add(pictureBox); // Добавляет PictureBox в FlowLayoutPanel
-                imagemove.AttachHandlers(pictureBox);
-            }
+            Database database = new Database();
+            tierlistManager = new TierlistManager(database);
+            tierlistManager.InitializeTierList(tierListTable, image_container);
         }
         private void Назад(object sender, EventArgs e)
         {
@@ -45,120 +23,27 @@ namespace Руководство
             this.Hide();
             oldForm.ShowDialog();
         }
+
         private void deleteButton_Click(object sender, EventArgs e)
         {
-            if (tierListTable.RowCount > 0)
-            {
-                int rowCount = tierListTable.RowCount;
-                // Удаление содержимого последней строки
-                for (int col = 0; col < tierListTable.ColumnCount; col++)
-                {
-                    Control controlToRemove = tierListTable.GetControlFromPosition(col, rowCount - 1);
-                    if (controlToRemove is FlowLayoutPanel)
-                    {
-                        FlowLayoutPanel panel = controlToRemove as FlowLayoutPanel;
-                        foreach (Control item in panel.Controls)
-                        {
-                            if (item is PictureBox)
-                            {
-                                PictureBox picture = item as PictureBox;
-                                // Возврат PictureBox в image_container
-                                image_container.Controls.Add(picture);
-                            }
-                        }
-                    }
-                    tierListTable.Controls.Remove(controlToRemove);
-                }
-                // Удаление последней строки
-                tierListTable.RowStyles.RemoveAt(rowCount - 1);
-                tierListTable.RowCount--;
-                // Пересчитывает размеры строк в таблице
-                for (int i = 0; i < tierListTable.RowCount; i++)
-                {
-                    tierListTable.RowStyles[i].SizeType = SizeType.Absolute;
-                    tierListTable.RowStyles[i].Height = 130; // Устанавливаем желаемую высоту строки
-                }
-                // Обновление размеров контейнера
-                tierListTable.AutoScrollMinSize = new Size(0, tierListTable.RowCount * 130); // Учитываем новое количество строк
-            }
+            tierlistManager.DeleteRow(tierListTable, image_container);
         }
+
         private void addButton_Click(object sender, EventArgs e)
         {
-            tierListTable.Height += 130;
-            // Создание новой строки с ячейками
-            int rowCount = tierListTable.RowCount;
-            tierListTable.RowCount++;
-            tierListTable.RowStyles.Add(new RowStyle(SizeType.Absolute, 130));
-            // Создаем новый RichTextBox и настраиваем ее
-            var richTextBox = new RichTextBox();
-            RichTextBoxInst(richTextBox);
-            // Добавляем новую RichTextBox в ячейку и добавляем ячейку в строку
-            tierListTable.Controls.Add(richTextBox, 0, rowCount);
-            var flowLayoutPanel = new FlowLayoutPanel();
-            flowLayoutPanel.FlowDirection = FlowDirection.LeftToRight;
-            flowLayoutPanel.WrapContents = true;
-            flowLayoutPanel.Size = new Size(501, 124);
-            string flowLayoutPanelName = "flowLayoutPanel" + rowCount; // rowCount - индекс новой строки
-            // Присваиваем имя
-            flowLayoutPanel.Name = flowLayoutPanelName;
-            // Добавляем новый FlowLayoutPanel во второй столбец последней строки
-            tierListTable.Controls.Add(flowLayoutPanel, 1, rowCount);
+            tierlistManager.AddRow(tierListTable);
         }
 
-        public void limitation()
-        {
-            foreach (Control control in tierListTable.Controls) // создает ограничение 25 симв
-            {
-                if (control is RichTextBox)
-                {
-                    ((RichTextBox)control).KeyPress += new KeyPressEventHandler(richTextBox_KeyPress);
-                }
-            }
-        }
-
-        public void RichTextBoxInst(RichTextBox richTextBox)
-        {
-            richTextBox.Size = new Size(118, 124);
-            richTextBox.ReadOnly = false;
-            richTextBox.SelectionAlignment = HorizontalAlignment.Center;
-            richTextBox.ForeColor = SystemColors.Window;
-            richTextBox.BackColor = SystemColors.ActiveCaption;
-            richTextBox.Font = new Font("Microsoft Sans Serif", 14.25f, FontStyle.Bold);
-            richTextBox.Text = "Введите новое название";
-            richTextBox.KeyPress += new KeyPressEventHandler(richTextBox_KeyPress);
-            richTextBox.BorderStyle = BorderStyle.None;
-        }
-        private void richTextBox_KeyPress(object sender, KeyPressEventArgs e)
-        {
-            RichTextBox richTextBox = (RichTextBox)sender;
-            // Проверка, что длина текста не превышает 25 символов
-            if (richTextBox.Text.Length >= 25 && e.KeyChar != (char)Keys.Back)
-            {
-                e.Handled = true; // Отмена ввода символа, если длина превышает 25 символов
-            }
-        }
         private void Reboot_Click(object sender, EventArgs e)
         {
             this.Controls.Clear();
             InitializeComponent();
-            imagemove = new imageMove(tierListTable, image_container);
-            tierListTable.Focus();
-            typeof(TableLayoutPanel).GetProperty("DoubleBuffered", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance).SetValue(tierListTable, true, null);
-            limitation();
-            selectChar();
+            tierlistManager.Reboot(tierListTable, image_container);
         }
+
         private void Save_Click(object sender, EventArgs e)
         {
-            SaveFileDialog saveFileDialog = new SaveFileDialog();
-            saveFileDialog.Filter = "PNG файлы (*.png)|*.png|Все файлы (*.*)|*.*";
-            if (saveFileDialog.ShowDialog() == DialogResult.OK)
-            {
-                Bitmap bmp = new Bitmap(tierListTable.Width, tierListTable.Height);
-                tierListTable.DrawToBitmap(bmp, new Rectangle(0, 0, tierListTable.Width, tierListTable.Height));
-                bmp.Save(saveFileDialog.FileName, ImageFormat.Png);
-                bmp.Dispose();
-                MessageBox.Show("Изображение успешно сохранено.", "Сохранение завершено", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            }
+            tierlistManager.SaveTierList(tierListTable);
         }
     }
 }
---
+++ Руководство/TierlistManager.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing.Imaging;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Руководство
+{
+    internal class TierlistManager
+    {
+        private CharacterRepository characterRepository;
+        private imageMove imagemove;
+        private Database database;
+
+        public TierlistManager(Database database)
+        {
+            this.database = database;
+            this.characterRepository = new CharacterRepository(database);
+        }
+        public void InitializeTierList(TableLayoutPanel tierListTable, FlowLayoutPanel imageContainer)
+        {
+            imagemove = new imageMove(tierListTable, imageContainer);
+            tierListTable.Focus();
+            typeof(TableLayoutPanel).GetProperty("DoubleBuffered", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance).SetValue(tierListTable, true, null);
+            Limitation(tierListTable);
+            SelectChar(imageContainer);
+        }
+
+        public void SelectChar(FlowLayoutPanel imageContainer)
+        {
+            DataTable charImage = characterRepository.Getpic();
+
+            foreach (DataRow row in charImage.Rows)
+            {
+                string imagePath = row["image"].ToString();
+                PictureBox pictureBox = new PictureBox
+                {
+                    Image = Image.FromFile(imagePath),
+                    SizeMode = PictureBoxSizeMode.Zoom,
+                    Width = 118,
+                    Height = 118,
+                    BorderStyle = BorderStyle.FixedSingle
+                };
+                imageContainer.Controls.Add(pictureBox);
+                imagemove.AttachHandlers(pictureBox);
+            }
+        }
+        public void Limitation(TableLayoutPanel tierListTable)
+        {
+            foreach (Control control in tierListTable.Controls)
+            {
+                if (control is RichTextBox)
+                {
+                    ((RichTextBox)control).KeyPress += new KeyPressEventHandler(RichTextBox_KeyPress);
+                }
+            }
+        }
+
+        public void AddRow(TableLayoutPanel tierListTable)
+        {
+            tierListTable.Height += 130;
+            int rowCount = tierListTable.RowCount;
+            tierListTable.RowCount++;
+            tierListTable.RowStyles.Add(new RowStyle(SizeType.Absolute, 130));
+
+            var richTextBox = new RichTextBox();
+            RichTextBoxInst(richTextBox);
+            tierListTable.Controls.Add(richTextBox, 0, rowCount);
+
+            var flowLayoutPanel = new FlowLayoutPanel
+            {
+                FlowDirection = FlowDirection.LeftToRight,
+                WrapContents = true,
+                Size = new Size(501, 124),
+                Name = "flowLayoutPanel" + rowCount
+            };
+            tierListTable.Controls.Add(flowLayoutPanel, 1, rowCount);
+        }
+
+        public void DeleteRow(TableLayoutPanel tierListTable, FlowLayoutPanel imageContainer)
+        {
+            if (tierListTable.RowCount > 0)
+            {
+                int rowCount = tierListTable.RowCount;
+                for (int col = 0; col < tierListTable.ColumnCount; col++)
+                {
+                    Control controlToRemove = tierListTable.GetControlFromPosition(col, rowCount - 1);
+                    if (controlToRemove is FlowLayoutPanel panel)
+                    {
+                        foreach (Control item in panel.Controls)
+                        {
+                            if (item is PictureBox picture)
+                            {
+                                imageContainer.Controls.Add(picture);
+                            }
+                        }
+                    }
+                    tierListTable.Controls.Remove(controlToRemove);
+                }
+                tierListTable.RowStyles.RemoveAt(rowCount - 1);
+                tierListTable.RowCount--;
+                for (int i = 0; i < tierListTable.RowCount; i++)
+                {
+                    tierListTable.RowStyles[i].SizeType = SizeType.Absolute;
+                    tierListTable.RowStyles[i].Height = 130;
+                }
+                tierListTable.AutoScrollMinSize = new Size(0, tierListTable.RowCount * 130);
+            }
+        }
+        public void SaveTierList(TableLayoutPanel tierListTable)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog
+            {
+                Filter = "PNG файлы (*.png)|*.png|Все файлы (*.*)|*.*"
+            };
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                Bitmap bmp = new Bitmap(tierListTable.Width, tierListTable.Height);
+                tierListTable.DrawToBitmap(bmp, new Rectangle(0, 0, tierListTable.Width, tierListTable.Height));
+                bmp.Save(saveFileDialog.FileName, ImageFormat.Png);
+                MessageBox.Show("Изображение успешно сохранено.", "Сохранение завершено", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+        public void Reboot(TableLayoutPanel tierListTable, FlowLayoutPanel imageContainer)
+        {
+            InitializeTierList(tierListTable, imageContainer);
+        }
+
+        private void RichTextBoxInst(RichTextBox richTextBox)
+        {
+            richTextBox.Size = new Size(118, 124);
+            richTextBox.ReadOnly = false;
+            richTextBox.SelectionAlignment = HorizontalAlignment.Center;
+            richTextBox.ForeColor = SystemColors.Window;
+            richTextBox.BackColor = SystemColors.ActiveCaption;
+            richTextBox.Font = new Font("Microsoft Sans Serif", 14.25f, FontStyle.Bold);
+            richTextBox.Text = "Введите новое название";
+            richTextBox.KeyPress += new KeyPressEventHandler(RichTextBox_KeyPress);
+            richTextBox.BorderStyle = BorderStyle.None;
+        }
+
+        private void RichTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            RichTextBox richTextBox = (RichTextBox)sender;
+            if (richTextBox.Text.Length >= 25 && e.KeyChar != (char)Keys.Back)
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
